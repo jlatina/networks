1fbb6518cf6f31f7d570a9cbd38ac943eebf0fe5
jlatina <80787313+jlatina@users.noreply.github.com>
2023-04-23 19:17:21 -0400

    Add files via upload

---
+++ get_file_sftp.py
@@ -0,0 +1,20 @@
+# Copyright 2023 Jeanette Villanueva jivillan@bu.edu
+
+"""
+UDP client - IPv4
+"""
+import sys
+import socket as s
+import os
+import hashlib
+
+# create client socket 
+client_soc = s.socket(s.AF_INET, s.SOCK_DGRAM)
+filename = sys.argv[1]
+request_msg = "GET {}".format(filename) + " "
+
+# encode the message before sending it 
+bstring = request_msg.encode()
+
+# default IP addr of server & port 
+client_soc.sendto(bstring, ("127.0.0.1",9000))
---
+++ serve_file_sftp.py
@@ -0,0 +1,36 @@
+# Copyright 2023 Jeanette Villanueva jivillan@bu.edu
+
+"""
+UDP server - IPv4
+"""
+import sys
+import socket as s
+import os
+import hashlib
+from get_file_sftp import bstring, filename
+
+
+# create server socket 
+server_soc = s.socket(s.AF_INET, s.SOCK_DGRAM)
+
+# list to store responses of server 
+messages = ['FOUND', 'BADREQUEST', 'TOOLARGE', 'NOTFOUND']
+
+
+while True:
+    message, address = server_soc.recvfrom(1024)
+    normalstr = bstring.decode()
+    
+    # try & catch for opening the file 
+
+    file_size = os.stat(filename).st_size
+
+    with open(filename,'rb') as f:
+        contents = f.read()
+
+    # check if it's in the directory
+    files = os.listdir()
+
+    h=hashlib.md5()
+    h.update(contents)
+    md5sum = h.hexdigest()
